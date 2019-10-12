a51317901974c853e2729d3d34feb00c373e6960
César Bolaños <cesar.bolanos.andino@gmail.com>
2019-10-12 09:32:44 -0600

    Set coordinates to background image

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -6,15 +6,14 @@ class MainMenu extends Phaser.Scene {
   }
 
   preload() {
-    console.log(4 * 8);
     this.load.image('mainmenu-background', 'assets/mainmenu/background.jpg');
     this.load.css('80s', 'assets/mainmenu/css/index.css');
   }
 
   create() {
-    const background = this.add.image('mainmenu-background');
-    background.displayWidth = this.game.config.width;
-    background.displayHeight = this.game.config.height;
+    const background = this.add.image(400, 300, 'mainmenu-background');
+    background.displayWidth = window.GAME.game.config.width;
+    background.displayHeight = window.GAME.game.config.height;
 
     const playButton = document.createElement('input');
     playButton.className = 'play';
